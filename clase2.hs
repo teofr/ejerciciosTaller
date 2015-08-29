5517464e76489bdcc9c6748ab2892e13464d9e46
Teodoro Freund <iteofreund@gmail.com>
2015-08-29 19:32:23 -0300

    HomeWork for 2nd class

---
+++ clase2.hs
@@ -0,0 +1,9 @@
+import Data.Char
+
+pendiente :: (Float,Float) -> (Float,Float) -> Float
+
+pendiente p1 p2 = (snd p2 - snd p1)/ (fst p2 - fst p1)
+
+iniciales :: String -> String -> String
+
+iniciales no ap = [head no, '.', head ap,'.']
